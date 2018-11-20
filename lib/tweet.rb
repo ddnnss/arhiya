9d729f2de4cb74954f2d2283370c7b13f5ec6642
qqw  wee <a@a.ru>
2018-11-20 16:25:59 +0300

    1

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -2,6 +2,12 @@ require 'discordrb/webhooks'
 require 'net/http'
 require 'json'
 
+WEBHOOK_URL = 'https://discordapp.com/api/webhooks/514046082834235402/6njUGx1IqlA99-_VMsp5CEOzjozQc4yvS88jrpmmipQ2EqJqfpHB7KvoUW1pY738vZ0L'
+VK_URL = 'https://api.vk.com/method/wall.get?domain=fallout&count=1&access_token=4195381c4195381c4195381c5141f2245f441954195381c1a66deb89cffcc7d165f14b3&v=5.87'
+@last_id = 0
+@client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
+@timer = 1800
+
 if (Gem.win_platform?)
   Encoding.default_external = Encoding.find(Encoding.locale_charmap)
   Encoding.default_internal = __ENCODING__
@@ -11,30 +17,48 @@ if (Gem.win_platform?)
   end
 end
 
-WEBHOOK_URL = 'https://discordapp.com/api/webhooks/514046082834235402/6njUGx1IqlA99-_VMsp5CEOzjozQc4yvS88jrpmmipQ2EqJqfpHB7KvoUW1pY738vZ0L'
-client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
-
-
-
-url = 'https://api.vk.com/method/wall.get?domain=fallout&count=1&access_token=4195381c4195381c4195381c5141f2245f441954195381c1a66deb89cffcc7d165f14b3&v=5.87'
-uri = URI(url)
-response = Net::HTTP.get(uri)
-parced = JSON.parse(response)
-
-p  parced['response']['items'][0]['id']
-p  parced['response']['items'][0]['text']
-p  parced['response']['items'][0]['attachments'].first['photo']['sizes'][6]['url']
-
-
-
-client.execute do |builder|
-  builder.content = parced['response']['items'][0]['text']
-  builder.add_embed do |embed|
-  #  embed.title = 'Embed title'
-  #  embed.description = 'Embed description'
-    embed.image = Discordrb::Webhooks::EmbedImage.new(url: parced['response']['items'][0]['attachments'].first['photo']['sizes'][6]['url'])
-  #  embed.timestamp = Time.now
+def get_and_send
+  uri = URI(VK_URL)
+  response = Net::HTTP.get(uri)
+  parced = JSON.parse(response)
+  if @last_id == parced['response']['items'][0]['id'].to_i
+    p 'Нет новых постов...'
+    p '---------------------------------------------------------------------'
+    return
+  else
+    p 'Есть новый пост ID : ' + parced['response']['items'][0]['id'].to_s
+    p 'Подготовка отправки сообщения в дискорд...'
+    @client.execute do |builder|
+      builder.content = parced['response']['items'][0]['text']
+      builder.add_embed do |embed|
+        embed.image = Discordrb::Webhooks::EmbedImage.new(url: parced['response']['items'][0]['attachments'].first['photo']['sizes'][6]['url'])
+      end
+    end
+    p 'Сообщение отправлено...'
+    p '---------------------------------------------------------------------'
+    @last_id = parced['response']['items'][0]['id'].to_i
   end
+
+
 end
+
+loop do
+  p '---------------------------------------------------------------------'
+  p 'Парсим ВК...'
+  get_and_send()
+  sleep(@timer)
+
+end
+
+
+
+
+
+# parced['response']['items'][0]['id']
+#  parced['response']['items'][0]['text']
+#  parced['response']['items'][0]['attachments'].first['photo']['sizes'][6]['url']
+
+
+
 
 
