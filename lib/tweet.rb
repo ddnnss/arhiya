4f2c17f25f02a16a028d1fd5293f095f22570819
qqw  wee <a@a.ru>
2018-11-21 16:02:01 +0300

    1

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -20,7 +20,7 @@ ActiveRecord::Base.establish_connection(
 )
 
 WEBHOOK_URL = 'https://discordapp.com/api/webhooks/514046082834235402/6njUGx1IqlA99-_VMsp5CEOzjozQc4yvS88jrpmmipQ2EqJqfpHB7KvoUW1pY738vZ0L'
-VK_URL = 'https://api.vk.com/method/wall.get?domain=scum_survival&count=1&access_token=4195381c4195381c4195381c5141f2245f441954195381c1a66deb89cffcc7d165f14b3&v=5.87'
+VK_URL = 'https://api.vk.com/method/wall.get?domain=scum_survival&count=1&offset=1&access_token=4195381c4195381c4195381c5141f2245f441954195381c1a66deb89cffcc7d165f14b3&v=5.87'
 @last_id = 0
 @client = Discordrb::Webhooks::Client.new(url: WEBHOOK_URL)
 @timer = 3600
@@ -40,18 +40,33 @@ def get_and_send
   parced = JSON.parse(response)
   vk_test = Vknews.find_by_news_id(parced['response']['items'][0]['id'])
   if vk_test
-    p 'Нет новых постов...'
+    p 'NO NEW POSTS...'
     p '---------------------------------------------------------------------'
     return
   else
     p 'Есть новый пост ID : ' + parced['response']['items'][0]['id'].to_s
     vk = Vknews.new
+    if  parced['response']['items'][0]['attachments'][0]['type'] == 'photo'
+      p 'PHOTO POST...'
+      vk.news_image = parced['response']['items'][0]['attachments'][0]['photo']['sizes'][6]['url']
+      vk.news_name = parced['response']['items'][0]['text'].truncate(40, separator: ' ')
+    end
+    if  parced['response']['items'][0]['attachments'][0]['type'] == 'link'
+      p 'LINK POST...'
+      vk.news_image = parced['response']['items'][0]['attachments'][0]['link']['photo']['sizes'][6]['url']
+      vk.news_name = parced['response']['items'][0]['attachments'][0]['link']['title']
+    end
+    if  parced['response']['items'][0]['attachments'][0]['type'] == 'video'
+      p 'VIDEO POST...'
+      vk.news_image = parced['response']['items'][0]['attachments'][0]['video']['photo_640']
+      vk.news_name = parced['response']['items'][0]['text'].truncate(40, separator: ' ')
+    end
     vk.news_id = parced['response']['items'][0]['id']
-    vk.news_image = parced['response']['items'][0]['attachments'].first['photo']['sizes'][6]['url']
-    vk.news_name = parced['response']['items'][0]['text'].truncate(40, separator: ' ')
+
     vk.news_link = 'https://vk.com/scum_survival?w=wall'+ parced['response']['items'][0]['from_id'].to_s + '_' + parced['response']['items'][0]['id'].to_s
     vk.save
-    p 'Сообщение записано в БД...'
+    p 'SAVED IN DB...'
+    p 'WAITING NEXT TRY...'
   #  p 'Подготовка отправки сообщения в дискорд...'
   #  @client.execute do |builder|
   #    builder.content = parced['response']['items'][0]['text']
@@ -59,17 +74,17 @@ def get_and_send
   #      embed.image = Discordrb::Webhooks::EmbedImage.new(url: parced['response']['items'][0]['attachments'].first['photo']['sizes'][6]['url'])
   #    end
    end
-    p 'Сообщение отправлено...'
+   # p 'MESSAGE SENT IN DISCORD...'
     p '---------------------------------------------------------------------'
     @last_id = parced['response']['items'][0]['id'].to_i
   end
 
 
 
-
+p 'INIT...'
 loop do
   p '---------------------------------------------------------------------'
-  p 'Парсим ВК...'
+  p 'PARSING VK...'
   get_and_send()
   sleep(@timer)
 
@@ -77,13 +92,24 @@ end
 
 
 
+#uri = URI(VK_URL)
+#response = Net::HTTP.get(uri)
+#parced = JSON.parse(response)
 
-
+#if  parced['response']['items'][0]['attachments'][0]['type'] == 'photo'
+ # p  parced['response']['items'][0]['attachments'][0]['photo']['sizes'][6]['url']
+#end
+#if  parced['response']['items'][0]['attachments'][0]['type'] == 'link'
+ # p  parced['response']['items'][0]['attachments'][0]['link']['photo']['sizes'][6]['url']
+#end
+#if  parced['response']['items'][0]['attachments'][0]['type'] == 'video'
+#  p  parced['response']['items'][0]['attachments'][0]['video']['photo_640']
+#end
 #p parced['response']['items'][0]['id']
 #p parced['response']['items'][0]['from_id']
 #p parced['response']['items'][0]['text']
 #p parced['response']['items'][0]['text'].truncate(40, separator: ' ')
-#p  parced['response']['items'][0]['attachments'].first['photo']['sizes'][6]['url']
+
 #p 'https://vk.com/scum_survival?w=wall'+ parced['response']['items'][0]['from_id'].to_s + '_' + parced['response']['items'][0]['id'].to_s
 
 
